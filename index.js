02f9c9ca138cc9b6def53a1c8614cc896006979f
Yousuf Kalim <yousufkalim@outlook.com>
2020-10-31 23:05:41 +0500

    Change views dir name in index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const users = require('./Controllers/users');
 
 //EJS stuff
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "Views"));
 
 //Middleware
 app.use("/public", express.static("public"));
